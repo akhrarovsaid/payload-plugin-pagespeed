4160f530bb40568639df9d181c1a80ea9bd1a6d6
akhrarovsaid <akhrarovsaid@gmail.com>
2025-08-20 16:02:07 -0400

    chore: install dotenv for e2e test runs

--- playwright.config.js
+++ playwright.config.js
@@ -1,12 +1,12 @@
 import { defineConfig, devices } from '@playwright/test'
+import dotenv from 'dotenv'
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+dotenv.config({
+  path: path.resolve(dirname(fileURLToPath(import.meta.url)), './dev/.env'),
+  quiet: true,
+})
 
 /**
  * See https://playwright.dev/docs/test-configuration.
