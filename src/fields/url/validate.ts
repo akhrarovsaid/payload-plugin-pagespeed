81e2256341538cfc4273f65833bac47fdf233540
akhrarovsaid <akhrarovsaid@gmail.com>
2025-08-24 17:16:24 -0400

    fix: allow localhost in pattern

--- src/fields/url/validate.ts
+++ src/fields/url/validate.ts
@@ -2,7 +2,8 @@ import type { TextFieldSingleValidation } from 'payload'
 
 import { text } from 'payload/shared'
 
-const pattern = /^(?:https?:\/\/)?(?:[\w-]+\.)+[a-z]{2,}(?:\/\S*)?$/i
+const pattern =
+  /^(?:https?:\/\/)?(?:localhost|\d{1,3}(?:\.\d{1,3}){3}|(?:[\w-]+\.)+[a-z]{2,})(?::\d{1,5})?(?:\/\S*)?$/i
 
 export const validate: TextFieldSingleValidation = (value, options) => {
   const { req } = options
